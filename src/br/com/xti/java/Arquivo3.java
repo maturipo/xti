28863f98eccad06724c0808a6cfb9952cc834e30
Marcos Tulio <marcostulio.brz@gmail.com>
2016-01-23 01:13:47 -0400

    Adicionado a classe Arquivo3

---
+++ src/br/com/xti/java/Arquivo3.java
@@ -0,0 +1,40 @@
+package br.com.xti.java;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Arquivo3 {
+
+	//A partir do Java 7 faz assim:
+	public static void main(String[] args) {
+		Path path = Paths.get("/home/tulio/Copy/workspace/xti/files/texto.txt");
+		Charset cs = StandardCharsets.UTF_8;
+
+		//Escrita
+		/*try (BufferedWriter w = Files.newBufferedWriter(path, cs)) {
+			w.write("Texto 1\n");
+			w.write("Texto 2\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
+		
+		//Leitura
+		try (BufferedReader r = Files.newBufferedReader(path, cs)) {
+			String line = null;
+			while ((line = r.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+	}
+
+}
