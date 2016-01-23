2d95b877e73bbf675bf86caae9222c6a7408e661
Marcos Tulio <marcostulio.brz@gmail.com>
2016-01-23 01:20:50 -0400

    Arquivo2 atualizado

--- src/br/com/xti/java/Arquivo2.java
+++ src/br/com/xti/java/Arquivo2.java
@@ -1,16 +1,28 @@
 package br.com.xti.java;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Arquivo2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//Antes se fazia assim: (Ver Arquivo2.java)
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get("/home/tulio/Copy/workspace/xti/files/texto.txt");
+		Charset cs = StandardCharsets.UTF_8;
 		
-		/*
-		 * 
-		 * Implementar 2*/
-
-		//Implementar 3
+		
+		BufferedWriter bw = Files.newBufferedWriter(path, cs);
+		bw.write("Texto 1");
+		bw.write("Texto 2");
+		bw.write("Texto 3");
+		
+		bw.flush();
+		bw.close();
 		
 	}
 
